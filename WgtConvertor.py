e77a9a4b17ff33157743dd65c28db967e1c35959
AbhishekHPatil <abhishek.hpatil63@gmail.com>
2024-01-24 12:08:24 +0530

    created new branch for exercise 1.3

--- WgtConvertor.py
+++ WgtConvertor.py
@@ -1,4 +1,5 @@
 name = input("What is your name? ")
+print("Are you ready to convert your weight in kgs?")
 weight_pounds = input("What is your weight in pounds? ")
 weight_kgs = float(weight_pounds) / 2.205
 print(f"{name} your weight in kilograms is {weight_kgs}")
