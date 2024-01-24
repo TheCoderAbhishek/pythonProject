9396a5f40e1a545dbc45e84c6b5e0597f61fa675
AbhishekHPatil <abhishek.hpatil63@gmail.com>
2024-01-24 12:05:21 +0530

    new feature for convert weight from pounds to kilograms.

---
+++ WgtConvertor.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+weight_pounds = input("What is your weight in pounds? ")
+weight_kgs = float(weight_pounds) / 2.205
+print(f"{name} your weight in kilograms is {weight_kgs}")
