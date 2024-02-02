eb3d8c1804b3029a6fbb334ec27b13113337d6ec
AbhishekHPatil <abhishek.hpatil63@gmail.com>
2024-02-02 13:40:26 +0530

    add new file to detect weather depends upon temperature.

---
+++ weatherDetect.py
@@ -0,0 +1,15 @@
+current_temperature = int(input("What is today's temperature (in degree celsius)? "))
+
+# if current_temperature > 30:
+#     print("It's a hot day!")
+# elif current_temperature < 10:
+#     print("It's a cold day!")
+# else:
+#     print("Enjoy Day! It's a good day.")
+
+if 30 > current_temperature > 10:
+    print("Enjoy Day! It's a good day.")
+elif current_temperature > 30:
+    print("It's a hot day!")
+else:
+    print("It's a cold day!")
