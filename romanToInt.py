d0566d5a923d14bd1dc0a22fc26164f6eb9d4e0a
AbhishekHPatil <abhishek.hpatil63@gmail.com>
2024-02-12 14:14:17 +0530

    Roman number converted into arithmatic number.

---
+++ romanToInt.py
@@ -0,0 +1,25 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        prev_value = 0
+
+        for char in s:
+            value = roman_values[char]
+            if value > prev_value:
+                total += value - 2 * prev_value
+            else:
+                total += value
+            prev_value = value
+
+        return total
+
+
+# Create an instance of the Solution class
+sol = Solution()
+
+# Get input from the user
+roman_numeral = input("Enter a Roman numeral: ")
+
+# Convert the Roman numeral to an integer and print the result
+print("The integer value of the Roman numeral", roman_numeral, "is:", sol.romanToInt(roman_numeral))
